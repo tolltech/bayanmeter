ce97d19b602cb3de531ad119f5a4436e67561c15
tolltech <alexandrovpe@gmail.com>
2020-09-28 22:30:27 +0400

    mighration

---
+++ Migration/ChatDto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Migration
+{
+    public class ChatDto
+    {
+        //{
+        //    "name": "ВЕСЕЛУХА ДЛЯ СВОИХ",
+        //    "type": "private_supergroup",
+        //    "id": 9851555733,
+        //    "messages": [
+            //{
+            //    "id": 11546,
+            //    "type": "message",
+            //    "date": "2020-09-28T16:31:39",
+            //    "from": "Катя Зеленина",
+            //    "from_id": 167301535,
+            //    "photo": "photos/photo_5920@28-09-2020_16-31-39.jpg",
+            //    "width": 588,
+            //    "height": 590,
+            //    "text": ""
+            //}
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("messages")]
+            public MessageDto[] Messages { get; set; }
+    }
+
+    public class MessageDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("date")] 
+        public DateTime Date { get; set; }
+
+        [JsonProperty("from_id")]
+        public int FromUserId { get; set; }
+
+        [JsonProperty("photo")]
+        public string PhotoPath { get; set; }
+        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
--- Migration/Program.cs
+++ Migration/Program.cs
@@ -1,16 +1,23 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using Ninject;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Tolltech.BayanMeter.Psql;
 using Tolltech.BayanMeterLib.TelegramClient;
 using Tolltech.Core;
 using Tolltech.PostgreEF.Integration;
+using File = System.IO.File;
 
 namespace Migration
 {
     class Program
     {
+        private static readonly string input = @"ChatExport_2020-09-28/result.json";
+
         static void Main(string[] args)
         {
             var argsFileName = "args.txt";
@@ -30,7 +37,92 @@ namespace Migration
             kernel.Rebind<IConnectionString>().ToConstant(new ConnectionString(connectionString));
 
             var imageBayanService = kernel.Get<IImageBayanService>();
-            //client.GetChatAsync(new )
+            var chat1 = client.GetChatAsync(new ChatId(-1001261621141)).GetAwaiter().GetResult();
+
+            var inputText = File.ReadAllText(input);
+            var chat = JsonConvert.DeserializeObject<ChatDto>(inputText);
+
+            var total = chat.Messages.Length;
+            var current = 0;
+            var skip = 0;
+            var errors = 0;
+            var list = new List<Tolltech.BayanMeterLib.MessageDto>(chat.Messages.Length);
+            var users = new Dictionary<int, ChatMember>();
+
+            foreach (var message in chat.Messages)
+            {
+                ++current;
+                Console.WriteLine($"{current}/{total}");
+                if (string.IsNullOrWhiteSpace(message.PhotoPath))
+                {
+                    Console.WriteLine($"No Photo");
+                    ++skip;
+                    continue;
+                }
+
+                if (message.Type != "message")
+                {
+                    Console.WriteLine($"No Message");
+                    ++skip;
+                    continue;
+                }
+
+                var path = Path.Combine("../../../ChatExport_2020-09-28", message.PhotoPath);
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine($"No Photo exists");
+                    ++errors;
+                    ++skip;
+                    continue;
+                }
+
+                var photo = File.ReadAllBytes(path);
+                var now = DateTime.UtcNow;
+
+                var user = users.TryGetValue(message.FromUserId, out var cashUser)
+                    ? cashUser
+                    : GetUser(client, message);
+
+                users[message.FromUserId] = user;
+
+                list.Add(new Tolltech.BayanMeterLib.MessageDto
+                {
+                    ChatId = -1001261621141,
+                    CreateDate = now,
+                    EditDate = null,
+                    ForwardFromChatId = null,
+                    ForwardFromChatName = null,
+                    ForwardFromMessageId = 0,
+                    ForwardFromUserId = null,
+                    FromUserId = message.FromUserId,
+                    FromUserName = user?.User?.Username ?? string.Empty,
+                    ForwardFromUserName = null,
+                    ImageBytes = photo,
+                    IntId = message.Id,
+                    MessageDate = message.Date,
+                    StrId = $"-1001261621141_{message.Id}",
+                    Text = string.Empty,
+                    Timestamp = now.Ticks
+                });
+            }
+
+            Console.WriteLine($"WriteTODb {list.Count} records");
+            imageBayanService.SaveMessage(list.ToArray());
+
+            Console.WriteLine($"WriteTODb SUccess");
+        }
+
+        private static ChatMember GetUser(TelegramBotClient client, MessageDto message)
+        {
+            try
+            {
+                return client.GetChatMemberAsync(-1001261621141, message.FromUserId).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+                return null;
+            }
         }
     }
 }
